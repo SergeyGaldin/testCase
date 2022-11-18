7279aadcde504f445edfbdcbb2f727bd55c8e374
Sergey Galdin <veretennik-v@mail.ru>
2022-11-18 22:51:09 +0300

    refactoring signIn

--- app/src/main/java/com/example/testcase/MainActivity.kt
+++ app/src/main/java/com/example/testcase/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.testcase
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.SearchView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.app_bar_menu, menu)
+        val mSearch: MenuItem = menu.findItem(R.id.search)
+        val mSearchView: SearchView = mSearch.actionView as SearchView
+        mSearchView.isIconifiedByDefault = false
+        mSearchView.queryHint = "Поиск"
+        mSearchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+
+                return true
+            }
+        })
+        return super.onCreateOptionsMenu(menu)
     }
 }
